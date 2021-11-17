35c4b26ff3a38375bffd885117d46be7125c2af2
tford-dev <terranceford513@gmail.com>
2021-11-17 10:47:03 -0500

    bare minimum of header is done

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Header.css'
 import SearchIcon from '@mui/icons-material/Search';
+import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
 
 function Header() {
     return (
@@ -25,12 +26,17 @@ function Header() {
 
                 <div className="header__option">
                     <span className="header__optionLineOne">Returns</span>
-                    <span className="header__optionLineTwo">Orders</span>
+                    <span className="header__optionLineTwo">& Orders</span>
                 </div>
 
                 <div className="header__option">
                     <span className="header__optionLineOne">Your</span>
                     <span className="header__optionLineTwo">Prime</span>
+                </div>
+
+                <div className="header__optionBasket">
+                    <ShoppingBasketIcon />
+                    <span className="header__optionLineTwo header__basketCount">0</span>
                 </div>
             </div>
         </div>
