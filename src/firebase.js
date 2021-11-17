e69de64a873d6c84f3f662ab86a718554c33718d
tford-dev <terranceford513@gmail.com>
2021-11-17 10:13:17 -0500

    First commit, working on header and adding material-ui

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAneJiGWIUraTmwNmbprssYeW1p4a0DicM",
+    authDomain: "clone-18b36.firebaseapp.com",
+    projectId: "clone-18b36",
+    storageBucket: "clone-18b36.appspot.com",
+    messagingSenderId: "180197115911",
+    appId: "1:180197115911:web:535880609524d851f002a2",
+    measurementId: "G-1MKPSD559R"
+};
