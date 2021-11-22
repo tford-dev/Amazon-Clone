751125952923554decf5073d49e7f48e363f053d
tford-dev <terranceford513@gmail.com>
2021-11-22 10:27:33 -0500

    final commit before deploy attempt

--- src/Components/Checkout/Checkout.js
+++ src/Components/Checkout/Checkout.js
@@ -13,7 +13,7 @@ function Checkout() {
                 <img 
                     className="checkout__ad"
                     src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
-                    alt="image banner"
+                    alt="checkout banner"
                 />
 
                 <div>
--- src/firebase.js
+++ src/firebase.js
@@ -2,7 +2,6 @@
 import firebase from "firebase/compat/app"
 import "firebase/compat/auth"
 import "firebase/compat/firestore"
-import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
