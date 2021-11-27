abca10ea58af76c9fffb493c398cffc486462af8
tford-dev <terranceford513@gmail.com>
2021-11-27 17:40:38 -0500

    set up firebase deploy

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:5001/clone-18b36/us-central1/api' 
+    baseURL: 'https://us-central1-clone-18b36.cloudfunctions.net/api'
+    //Below is the local API endpoint for debugging purposes
+    //'http://localhost:5001/clone-18b36/us-central1/api' 
     //The api (cloud function url)
 });
 
