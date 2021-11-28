7ce7238062199c2b49f3defbf0d27a548d8f4859
tford-dev <terranceford513@gmail.com>
2021-11-28 10:25:23 -0500

    edited header styling

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -43,14 +43,14 @@ function Header() {
             </div>
 
             <div className="header__nav">
-                <Link to={!user && '/login'}>
+                <Link to={!user && '/login'} className="header__link">
                     <div onClick={handleAuthentication} className="header__option">
                         <span className="header__optionLineOne">Hello {!user ? 'Guest' : user.email}</span>
                         <span className="header__optionLineTwo">{user ? 'Sign Out' : 'Sign In'}</span>
                     </div>
                 </Link>
 
-                <Link to="/orders">
+                <Link to="/orders" className="header__link">
                     <div className="header__option">
                         <span className="header__optionLineOne">Returns</span>
                         <span className="header__optionLineTwo">& Orders</span>
